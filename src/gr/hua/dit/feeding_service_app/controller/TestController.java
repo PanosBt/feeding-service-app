a317c8ce659d567077b9a5aae342de71fdbf93a2
Panagiotis Boutas <panbout@hotmail.gr>
2018-12-18 00:17:15 +0200

    Added hibernate and properties file support

--- src/gr/hua/dit/feeding_service_app/controller/TestController.java
+++ src/gr/hua/dit/feeding_service_app/controller/TestController.java
@@ -1,20 +1,19 @@
-package gr.hua.dit.feeding_service_app.controllers;
+package gr.hua.dit.feeding_service_app.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class TestController {
-	
+
 	@RequestMapping("/")
-	public String showHome( ) {
+	public String showHome() {
 		return "home";
 	}
-	
+
 	@RequestMapping("/admin")
 	public String showAdminPage() {
 		return "admin";
 	}
-	
 
 }
