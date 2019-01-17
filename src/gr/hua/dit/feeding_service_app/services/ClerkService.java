e1113806b758409d4940f89c0e3d8b1c24df4dcb
Panagiotis Boutas <panbout@hotmail.gr>
2019-01-18 00:11:05 +0200

    created ClerkService just as a template

---
+++ src/gr/hua/dit/feeding_service_app/services/ClerkService.java
@@ -0,0 +1,7 @@
+package gr.hua.dit.feeding_service_app.services;
+
+
+//Will this be used? We shall see...
+public interface ClerkService {
+
+}
---
+++ src/gr/hua/dit/feeding_service_app/services/ClerkServiceImpl.java
@@ -0,0 +1,8 @@
+package gr.hua.dit.feeding_service_app.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClerkServiceImpl implements ClerkService {
+
+}
